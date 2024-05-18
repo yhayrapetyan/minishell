3dad53ced68c2b34db14f9fa056d9eeee38e19ca
Yura <211003yura@gmail.com>
2024-05-18 16:30:41 +0400

    add some libft functions

---
+++ src/libft/ft_isalpha.c
@@ -0,0 +1,9 @@
+#include "minishell.h"
+
+int	ft_isalpha(int c)
+{
+	if (!((c >= 'a' && c <= 'z')
+		  || (c >= 'A' && c <= 'Z')))
+		return (0);
+	return (1);
+}
---
+++ src/libft/ft_isdigit.c
@@ -0,0 +1,6 @@
+#include "minishell.h"
+
+int	ft_isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
