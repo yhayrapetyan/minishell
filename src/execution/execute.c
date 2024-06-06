f642a1911d87b7783fa202404408036adc6f3011
Yura <211003yura@gmail.com>
2024-06-06 15:16:05 +0400

    fix syntax err if input heredoc for builtin

--- src/execution/execute.c
+++ src/execution/execute.c
@@ -90,6 +90,8 @@ int	execute(t_data *data)
 		{
 			write(2, data->commands->err_message, ft_strlen(data->commands->err_message));
 			write(2, "\n", 1);
+			if (data->commands->is_input_heredoc)
+				unlink(data->commands->io_fds->infile);
 			return (get_exit_status(data->commands->err_type));
 		}
 		builtin_run(data);
