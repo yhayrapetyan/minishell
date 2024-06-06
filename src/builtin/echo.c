60c530e18ed8f07644ba007f0e60f579b0d2fd29
prestij-SK <steve.kedikian98@gmail.com>
2024-06-06 22:52:11 +0400

    adding -n option for echo.

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -12,10 +12,54 @@
 
 #include "builtin.h"
 
-int	builtin_echo(t_data *data)
+static int	has_n_option(t_data *data)
 {
 	int	i;
 
+	if (!data)
+		return (NOT_FOUND);
+	if (data->commands->args)
+	{
+		if (data->commands->args[0])
+		{
+			if (data->commands->args[1])
+			{
+				if (data->commands->args[1][0] == '-')
+				{
+					i = 1;
+					while (data->commands->args[1][i])
+					{
+						if (data->commands->args[1][i] != 'n')
+							return (NOT_FOUND);
+						++i;
+					}
+					return (EXIT_SUCCESS);
+				}
+			}
+		}
+	}
+	return (NOT_FOUND);
+}
+
+static void	print_echo(t_data *data, int start)
+{
+	int	i;
+
+	if (!data)
+		return ;
+	i = start;
+	while (data->commands->args[i])
+	{
+		write(STDOUT_FILENO, data->commands->args[i],
+			ft_strlen(data->commands->args[i]));
+		if (data->commands->args[i + 1])
+			write(STDOUT_FILENO, " ", 1);
+		++i;
+	}
+}
+
+int	builtin_echo(t_data *data)
+{
 	if (!data)
 	{
 		minishell_error("echo", "NULL", "Data error\n");
@@ -23,16 +67,15 @@ int	builtin_echo(t_data *data)
 	}
 	if (data->commands->args)
 	{
-		i = 1;
-		while (data->commands->args[i])
+		if (has_n_option(data) != NOT_FOUND)
+			print_echo(data, 2);
+		else
 		{
-			write(STDOUT_FILENO, data->commands->args[i],
-				ft_strlen(data->commands->args[i]));
-			if (data->commands->args[i + 1])
-				write(STDOUT_FILENO, " ", 1);
-			++i;
+			print_echo(data, 1);
+			write(STDOUT_FILENO, "\n", 1);
 		}
 	}
-	write(STDOUT_FILENO, "\n", 1);
+	else
+		write(STDOUT_FILENO, "\n", 1);
 	return (EXIT_SUCCESS);
 }
